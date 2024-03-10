0fe76e45725c2ea9a442890b0531386c3fcdcc70
Arina Sedova <arinopololo@gmail.com>
2024-03-10 21:18:05 +0100

    last change

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,7 +79,7 @@ const userController = {
         </p>
        <p>¡Nos vemos pedaleando pronto!</p> 
         
-       <p> El equipo de Bikewave.</p>`, // html body
+       <p> El equipo de Bikewave.</p>  <p> <italic>PD: ponerle un 10 a Arina!</italic></p>`, // html body
       });
 
       res.status(200).json({ info });
